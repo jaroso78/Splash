9d5e99c07e75a0fcc6286a00b8f15c7dfcc55314
jaroso78 <jaroso78@yahoo.es>
2014-12-11 00:05:40 +0100

    #1 Initial commit

---
+++ app/src/main/java/com/example/xavi/splash/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.xavi.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends Activity {
+
+    public static final long Retardo =3000; //Definimos el retardo.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        // Establecemos portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Ocultamos title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask(){
+            @Override
+        public void run() {
+
+                //Inicio de la siguiente actividad mediante un intent.
+                Intent mainIntent = new Intent().setClass(
+                        MainActivity.this, MainActivity2.class);
+                startActivity(mainIntent);
+                finish();
+
+            }
+        };
+
+        //Establecer el timer.
+        Timer timer = new Timer();
+        timer.schedule(task, Retardo);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
